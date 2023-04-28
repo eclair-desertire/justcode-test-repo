863109f817afc2150ee7f95e498f4a1a5cad3752
Alisher Ruziyev <eclair.desertire@gmail.com>
2023-04-28 20:12:34 +0600

    inited blog application, first model

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+class Post(models.Model):
+    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,verbose_name='Пользователь создавший пост')
+    title=models.CharField(verbose_name='Заголовок',max_length=255,default='',null=True,blank=True)
+    text=models.TextField(verbose_name='Описание')
+    date_post=models.DateTimeField(default=timezone.now,verbose_name='Дата создания поста')
+
+    class Meta:
+        verbose_name='Пост'
+        verbose_name_plural='Посты'
+
+    def __str__(self) -> str:
+        return self.title
+
+# Create your models here.
