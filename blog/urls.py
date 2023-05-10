118ef6eab446002b33fb5b2b9ae520bc14a98f6f
Alisher Ruziyev <eclair.desertire@gmail.com>
2023-05-10 19:22:30 +0600

    added post form

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, about, post_single
+from .views import index, about, post_single, post_form
 
 urlpatterns=[
     path('',index,name='index'),
     path('<int:pk>/',post_single,name='single'),
+    path('post/new/',post_form,name="post_form"),
     path('about/',about,name='about')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from blog.forms import PostForm
+from django.shortcuts import redirect
+from django.utils import timezone
 
 
 def index(request):
@@ -17,4 +20,17 @@ def post_single(request,pk):
     # Post.objects.get(pk=pk)
     p=get_object_or_404(Post.objects.all(),pk=pk)
     return render(request,'post_single.html',{'post':p})
+
+def post_form(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('single', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'post_add.html', {'form': form})
 # Create your views here.
