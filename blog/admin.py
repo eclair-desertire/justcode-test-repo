333a9811337f673b4b2969c24cc77e0a058101b8
Alisher Ruziyev <eclair.desertire@gmail.com>
2023-05-29 19:30:08 +0600

    customized admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Post, User, ProductCard
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display=('user','title','text','date_post')
+    list_filter=('user','date_post')
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(User)
 admin.site.register(ProductCard)
 # Register your models here.
