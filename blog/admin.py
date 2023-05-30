ebd7a1cb8455d5c5b8876eb431f27b024a19b1f4
Alisher Ruziyev <eclair.desertire@gmail.com>
2023-05-30 23:35:57 +0600

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from .models import Post, User, ProductCard
 
-class PostAdmin(admin.ModelAdmin):
-    list_display=('user','title','text','date_post')
-    list_filter=('user','date_post')
 
-admin.site.register(Post, PostAdmin)
-admin.site.register(User)
+
+admin.site.register(Post)
+
+class PostInline(admin.StackedInline):
+    model=Post
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display=['username','email','role']
+    inlines=[PostInline]
+
 admin.site.register(ProductCard)
 # Register your models here.
